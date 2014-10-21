5ef721de5139a54056d132a785d90f5f6cbfa233
Robin Ricard <ricard.robin@gmail.com>
2014-10-21 13:32:01 +0200

    Add missing file

---
+++ src/main/java/fr/insalyon/optimod/models/Roadmap.java
@@ -0,0 +1,62 @@
+package fr.insalyon.optimod.models;
+
+import java.util.TreeSet;
+
+/**
+ * Represents a Courier's Road map
+ */
+public class RoadMap {
+    TomorrowDeliveries mTomorrowDeliveries;
+    Courier mCourier;
+    TreeSet<TimeWindow> mTimeWindows = new TreeSet<TimeWindow>(TimeWindow.COMPARATOR);
+
+    /**
+     * Gets the tomorrow deliveries
+     * @return A TomorrowDeliveries
+     */
+    public TomorrowDeliveries getTomorrowDeliveries() {
+        return mTomorrowDeliveries;
+    }
+
+    void setTomorrowDeliveries(TomorrowDeliveries deliveries) {
+        mTomorrowDeliveries = deliveries;
+    }
+
+    /**
+     * Gets the courier
+     * @return A Courier
+     */
+    public Courier getCourier() {
+        return mCourier;
+    }
+
+    void setCourier(Courier courier) {
+        mCourier = courier;
+    }
+
+    /**
+     * Add a new time window
+     * @param tw A time window
+     */
+    public void addTimeWindow(TimeWindow tw) {
+        tw.setRoadMap(this);
+        mTimeWindows.add(tw);
+    }
+
+    /**
+     * Delete a time window
+     * @param tw A Time Window
+     */
+    public void deleteTimeWindow(TimeWindow tw) {
+        tw.setRoadMap(null);
+        mTimeWindows.remove(tw);
+    }
+
+    /**
+     * Returns a read-only list
+     * @return A list of TimeWondow
+     */
+    public final TreeSet<TimeWindow> getTimeWindows() {
+        return mTimeWindows;
+    }
+}
