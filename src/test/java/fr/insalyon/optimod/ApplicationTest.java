76b6f4bc6b3d502b8425358afde4f82586cca00a
Robin Ricard <ricard.robin@gmail.com>
2014-10-21 17:50:48 +0200

    First Testing

--- src/test/java/fr/insalyon/optimod/ApplicationTest.java
+++ src/test/java/fr/insalyon/optimod/ApplicationTest.java
@@ -3,12 +3,8 @@ package fr.insalyon.optimod;
 import junit.framework.TestCase;
 
 public class ApplicationTest extends TestCase {
-    @Override
-    public void setUp() throws Exception {
-        super.setUp();
-    }
-
-    public void test_something() throws Exception {
-        assertTrue(true); // yay !
+    public void test_getInstance() throws Exception {
+        assertTrue(Application.getInstance() != null);
+        assertEquals(Application.getInstance(), Application.getInstance());
     }
 }
