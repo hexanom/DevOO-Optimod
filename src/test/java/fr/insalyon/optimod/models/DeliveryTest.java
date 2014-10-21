0513f99f6e47b713dde0d95f334104a014da3c2b
Robin Ricard <ricard.robin@gmail.com>
2014-10-21 21:00:53 +0200

    Test Delivery

---
+++ src/test/java/fr/insalyon/optimod/models/DeliveryTest.java
@@ -0,0 +1,33 @@
+package fr.insalyon.optimod.models;
+
+import junit.framework.TestCase;
+
+public class DeliveryTest extends TestCase{
+    private Delivery mDelivery;
+
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        mDelivery = new Delivery("TestPayload");
+    }
+
+    public void testDefaultConstructor() throws Exception {
+        assertTrue(new Delivery().getPayload() != null);
+    }
+
+    public void test_getPayload() throws Exception {
+        assertEquals("TestPayload", mDelivery.getPayload());
+    }
+
+    public void test_getTomorrowDeliveries() throws Exception {
+        TomorrowDeliveries td = new TomorrowDeliveries();
+        td.addDelivery(mDelivery);
+        assertEquals(mDelivery.getTomorrowDeliveries(), td);
+    }
+
+    public void test_getCustomer() throws Exception {
+        Customer customer = new Customer();
+        customer.addDelivery(mDelivery);
+        assertEquals(mDelivery.getCustomer(), customer);
+    }
+}
