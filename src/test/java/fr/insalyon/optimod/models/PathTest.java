cea9a74618225a8ff85210ce05d17d1a263c9723
rombrune <romain.brunat@insa-lyon.fr>
2014-10-24 09:49:52 +0200

    update according to diagram changes

--- src/test/java/fr/insalyon/optimod/models/PathTest.java
+++ src/test/java/fr/insalyon/optimod/models/PathTest.java
@@ -13,41 +13,35 @@ public class PathTest extends TestCase {
         mPath = new Path();
     }
 
-    public void test_getTimeWindow() throws Exception {
-        TimeWindow tw = new TimeWindow();
-        mPath.setTimeWindow(tw);
-        assertEquals(mPath.getTimeWindow(), tw);
-    }
 
     public void test_getOrderedSections() throws Exception {
         assertNull(mPath.getOrderedSections());
-        test_Section_1 = new Section("BobStreet",5,5);
-        test_Section_2 = new Section("BobRoad",5,5);
+        test_Section_1 = new Section("BobStreet", 5, 5);
+        test_Section_2 = new Section("BobRoad", 5, 5);
         mPath.appendSection(test_Section_1);
         mPath.appendSection(test_Section_2);
-        assertEquals(mPath.getOrderedSections().size(),2);
+        assertEquals(mPath.getOrderedSections().size(), 2);
     }
 
     public void test_getOrigin() throws Exception {
         assertNull(mPath.getOrigin());
-        test_Section_1 = new Section("BobStreet",5,5);
+        test_Section_1 = new Section("BobStreet", 5, 5);
         mPath.appendSection(test_Section_1);
-        assertEquals(mPath.getOrigin(),test_Section_1);
+        assertEquals(mPath.getOrigin(), test_Section_1);
         mPath.removeOrigin();
         assertNull(mPath.getOrigin());
     }
 
     public void test_getDestination() throws Exception {
         assertNull(mPath.getDestination());
-        test_Section_1 = new Section("BobStreet",5,5);
-        test_Section_2 = new Section("BobRoad",5,5);
+        test_Section_1 = new Section("BobStreet", 5, 5);
+        test_Section_2 = new Section("BobRoad", 5, 5);
         mPath.appendSection(test_Section_1);
         mPath.appendSection(test_Section_2);
-        assertEquals(mPath.getDestination(),test_Section_2);
+        assertEquals(mPath.getDestination(), test_Section_2);
         mPath.removeDestination();
         assertNull(mPath.getDestination());
     }
-
 
 
 }
