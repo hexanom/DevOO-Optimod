8c9a9fb0f573c9f8770d392d2276aa43ea43e0b7
rombrune <romain.brunat@insa-lyon.fr>
2014-11-04 19:07:35 +0100

    Dijkstra Test on small XML file validating Dijkstra Algorithm

--- src/test/java/fr/insalyon/optimod/tsp/DijkstraAlgorithmTest.java
+++ src/test/java/fr/insalyon/optimod/tsp/DijkstraAlgorithmTest.java
@@ -6,6 +6,7 @@ import fr.insalyon.optimod.models.factories.XMLMapFactory;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.util.LinkedList;
 
 /**
@@ -16,7 +17,7 @@ public class DijkstraAlgorithmTest {
     private XMLMapFactory mMapFactory;
     @Before
     public void setUp() throws Exception {
-        String filename = "resources/tests/plan10x10.xml";
+        String filename = "resources/tests/plan9.xml";
         mMapFactory = new XMLMapFactory(filename);
     }
 
@@ -24,8 +25,24 @@ public class DijkstraAlgorithmTest {
     public void testWithXml() throws Exception {
         Map map = mMapFactory.create();
         DijkstraAlgorithm dijkstraAlgorithm = new DijkstraAlgorithm();
-        dijkstraAlgorithm.execute(map.getLocationByAddress("20"));
-        LinkedList<Location> path = dijkstraAlgorithm.getPath(map.getLocationByAddress("80"));
+        dijkstraAlgorithm.execute(map.getLocationByAddress("0"));
+        LinkedList<Location> path = dijkstraAlgorithm.getPath(map.getLocationByAddress("9"));
+        print_path(path);
+    }
+
+    public void print_path(LinkedList<Location> path) {
+        int i = 0;
+
+        while (i < 9) {
+            try {
+
+                System.out.println(path.get(i).getAddress());
+                i++;
+            } catch (Exception e) {
+                System.out.println("Fin");
+                i++;
+            }
+        }
     }
 
 }
