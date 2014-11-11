4893eb66de2f7cbba5855a017287b7cd62cf5b40
Edouard Oger <edouard.oger@gmail.com>
2014-11-11 16:55:42 +0100

    Curved sections

---
+++ src/main/java/fr/insalyon/optimod/tsp/NoPathFoundException.java
@@ -0,0 +1,8 @@
+package fr.insalyon.optimod.tsp;
+
+/**
+ * Created by edouard on 11/11/14.
+ */
+public class NoPathFoundException extends Exception {
+    public NoPathFoundException(String message) { super(message); }
+}
--- src/main/java/fr/insalyon/optimod/views/SectionView.java
+++ src/main/java/fr/insalyon/optimod/views/SectionView.java
@@ -4,12 +4,14 @@ import fr.insalyon.optimod.models.Location;
 import fr.insalyon.optimod.models.Section;
 
 import java.awt.*;
+import java.awt.geom.QuadCurve2D;
 
 /**
  * Created by edouard on 10/11/14.
  */
 public class SectionView {
 
+    private static final int CURVATURE = 20;
     private static final int DEFAULT_WIDTH = 3;
     private static final int DEFAULT_BORDER_WIDTH = 2;
     public static final Color DEFAULT_COLOR = Color.WHITE;
@@ -46,22 +48,30 @@ public class SectionView {
         Color savedColor = g.getColor();
         Stroke savedStroke = g2d.getStroke();
 
-        if(mX1 == 384 && mY1 == 71 && mX2 == 467 && mY2 == 100) {
-            System.out.println("I am " + mColor);
-        }
+        // Normalized normal of the section !
+        double nX = - (mY2 - mY1);
+        double nY = mX2 - mX1;
+        double d = Math.sqrt((nX * nX) + (nY * nY));
+        nX /= d;
+        nY /= d;
+        //
+
+        Point center = getCenter();
+
+        QuadCurve2D curve = new QuadCurve2D.Float();
+        curve.setCurve(mX1, mY1, center.x + nX * CURVATURE, center.y + nY * CURVATURE, mX2, mY2);
 
         // Border
         g.setColor(mBorderColor);
         g2d.setStroke(new BasicStroke(mWidth + mBorderWidth));
-        g.drawLine(mX1, mY1, mX2, mY2);
+        g2d.draw(curve);
 
-        // Interior
+        //Interior
         g.setColor(mColor);
         g2d.setStroke(new BasicStroke(mWidth));
-        g.drawLine(mX1, mY1, mX2, mY2);
+        g2d.draw(curve);
 
         // Draw text
-        Point center = getCenter();
         g.setColor(mTextColor);
         g.drawString(mLabel, center.x, center.y);
 
@@ -71,6 +81,10 @@ public class SectionView {
 
     public Point getCenter() {
         return new Point((int) (((float)mX1 + mX2) / 2f), (int) (((float)mY1 + mY2) / 2f));
+    }
+
+    public Color getColor() {
+        return mColor;
     }
 
     public void setColor(Color color) {
