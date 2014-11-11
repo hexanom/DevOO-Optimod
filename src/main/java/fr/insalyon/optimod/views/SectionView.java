ae383f558269835b5a2a7c372a15b90c80dc9acd
Edouard Oger <edouard.oger@gmail.com>
2014-11-11 17:35:57 +0100

    Draw arrows on sections

--- src/main/java/fr/insalyon/optimod/views/SectionView.java
+++ src/main/java/fr/insalyon/optimod/views/SectionView.java
@@ -4,6 +4,8 @@ import fr.insalyon.optimod.models.Location;
 import fr.insalyon.optimod.models.Section;
 
 import java.awt.*;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Line2D;
 import java.awt.geom.QuadCurve2D;
 
 /**
@@ -11,9 +13,12 @@ import java.awt.geom.QuadCurve2D;
  */
 public class SectionView {
 
+    private static final double ARROW_PHI = Math.PI / 6d;
+    private static final double ARROW_LENGTH = 10;
+
     private static final int CURVATURE = 20;
     private static final int DEFAULT_WIDTH = 3;
-    private static final int DEFAULT_BORDER_WIDTH = 2;
+    private static final int DEFAULT_BORDER_WIDTH = 1;
     public static final Color DEFAULT_COLOR = Color.WHITE;
     private static final Color DEFAULT_BORDER_COLOR = Color.BLACK;
     private static final Color DEFAULT_TEXT_COLOR = Color.BLACK;
@@ -43,6 +48,10 @@ public class SectionView {
         mLabel = label;
     }
 
+    /**
+     * Paints the view
+     * @param g
+     */
     public void paint(Graphics g) {
         Graphics2D g2d = (Graphics2D) g;
         Color savedColor = g.getColor();
@@ -71,20 +80,62 @@ public class SectionView {
         g2d.setStroke(new BasicStroke(mWidth));
         g2d.draw(curve);
 
+        // Arrow head
+        g2d.setStroke(new BasicStroke());
+        g.setColor(Color.BLACK);
+        Point bezierMiddle = getBezierMiddle(curve);
+        drawArrow(g2d, Math.atan2(mY2 - mY1, mX2 - mX1), bezierMiddle.x, bezierMiddle.y);
+
         // Draw text
         g.setColor(mTextColor);
-        g.drawString(mLabel, center.x, center.y);
+        //g.drawString(mLabel, center.x, center.y);
 
         g.setColor(savedColor);
         g2d.setStroke(savedStroke);
     }
 
-    public Point getCenter() {
-        return new Point((int) (((float)mX1 + mX2) / 2f), (int) (((float)mY1 + mY2) / 2f));
+    /**
+     * Get the middle point coordinates of a curve
+     * Found on http://stackoverflow.com/questions/14174252
+     * @param curve
+     * @return
+     */
+    private Point getBezierMiddle(QuadCurve2D curve) {
+        double Dx = ( 0.5 * mX1 ) + (0.5 * curve.getCtrlX());
+        double Dy = ( 0.5 * mY1 ) + (0.5 * curve.getCtrlY());
+
+        double Ex = ( 0.5 * curve.getCtrlX() ) + (0.5 * mX2);
+        double Ey = ( 0.5 * curve.getCtrlY() ) + (0.5 * mY2);
+
+        double Px = ( 0.5 * Dx ) + (0.5 * Ex);
+        double Py = ( 0.5 * Dy ) + (0.5 * Ey);
+
+        return new Point((int)Px, (int)Py);
     }
 
-    public Color getColor() {
-        return mColor;
+    /** Draws an arrow head
+     * Found on http://www.coderanch.com/t/339505/GUI/java/drawing-arrows
+     * @param g2
+     * @param theta Rotation of the arrow
+     * @param x0 Arrow tip coord x
+     * @param y0 Arrow tip coord y
+     */
+    private void drawArrow(Graphics2D g2, double theta, double x0, double y0)
+    {
+        double x = x0 - ARROW_LENGTH * Math.cos(theta + ARROW_PHI);
+        double y = y0 - ARROW_LENGTH * Math.sin(theta + ARROW_PHI);
+        g2.draw(new Line2D.Double(x0, y0, x, y));
+        x = x0 - ARROW_LENGTH * Math.cos(theta - ARROW_PHI);
+        y = y0 - ARROW_LENGTH * Math.sin(theta - ARROW_PHI);
+        g2.draw(new Line2D.Double(x0, y0, x, y));
+    }
+
+    /**
+     * Get the center coordinates of the view
+     * @return The point of the center of the view
+     */
+    public Point getCenter() {
+        return new Point((int) (((float)mX1 + mX2) / 2f), (int) (((float)mY1 + mY2) / 2f));
     }
 
     public void setColor(Color color) {
